6d7ce374e7603795c74cac83b119cb5968be4a30
Hulivets Rostislav <hulivets.rostislav@gmail.com>
2021-04-19 00:40:30 +0300

    Update ChatRoomPage component

--- src/components/pages/ChatRoomPage/ChatRoomPage.tsx
+++ src/components/pages/ChatRoomPage/ChatRoomPage.tsx
@@ -1,9 +1,20 @@
 import React, { ReactElement } from 'react';
+import ChatMessagesList from '../../ui-kit/ChatMessagesList';
+import SubmitChatMessageForm from '../../forms/SubmitChatMessageForm';
 
-import './ChatRoomPage.scss'
+import './ChatRoomPage.scss';
+
+const MOCK = [{
+    id: '0',
+    userName: 'Test Name',
+    userMessage: 'Helooooo I am test user',
+}];
 
 const ChatRoomPage = (): ReactElement => (
-    <div className="chat-room-page">chat page</div>
+    <div className="chat-room-page">
+        <ChatMessagesList messages={MOCK} />
+        <SubmitChatMessageForm />
+    </div>
 );
 
 export default ChatRoomPage;
