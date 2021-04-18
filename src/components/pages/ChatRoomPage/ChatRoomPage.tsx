700f22f3ed36434fe5d038b6541c204cf57d65da
Hulivets Rostislav <hulivets.rostislav@gmail.com>
2021-04-18 20:22:47 +0300

    Remove form component from ChatRoomPage

--- src/components/pages/ChatRoomPage/ChatRoomPage.tsx
+++ src/components/pages/ChatRoomPage/ChatRoomPage.tsx
@@ -1,13 +1,9 @@
 import React, { ReactElement } from 'react';
-import LoginForm from '../../forms/LoginForm/LoginForm';
-import SubmitChatMessageForm from '../../forms/SubmitChatMessageForm/SubmitChatMessageForm';
 
 import './ChatRoomPage.scss'
 
 const ChatRoomPage = (): ReactElement => (
-    <div className="chat-room-page">
-        <SubmitChatMessageForm />
-    </div>
+    <div className="chat-room-page">chat page</div>
 );
 
 export default ChatRoomPage;
